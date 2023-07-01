44a187fa014da4d260815da56743ba7d0c50ac49
ggsong0328 <ggsong0328@naver.com>
2023-07-01 10:16:39 +0900

    1094

---
+++ 1094.py
@@ -0,0 +1,15 @@
+X = int(input())
+stick = 64
+save = []
+save.append(64)
+tmp = 0
+count = 0
+while sum(save) != X:
+    if sum(save) > X:
+        tmp = min(save) // 2
+        save.append(tmp)
+        save.remove(stick)
+        if sum(save) < X:
+            save.append(tmp)
+    stick = tmp
+print(len(save))  
